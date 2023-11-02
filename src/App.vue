4246ba6bddc93e088b3eff5f80950d2430d05b56
Allen <lcf7891@gmail.com>
2023-11-02 16:47:00 +0800

    建立專案、加入 bootstrap、基本設置

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<template>
+  <NavBar />
+
+  <router-view />
+
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
+
+<template>
+  <header>
+    <slot name="banner"></slot>
+  </header>
+
+  <main>
+    <h1 class="font-mantou text-primary-gradient">最新活動</h1>
+  </main>
+
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</template>
