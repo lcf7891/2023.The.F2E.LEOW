48aac0b6e23f1536ec930b5aafe8bed1d929ec64
Allen <lcf7891@gmail.com>
2023-11-14 16:32:33 +0800

    vue.config add publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/2023.The.F2E.LEOW/'
+  : '/',
   transpileDependencies: true,
 });
