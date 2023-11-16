53d92c8a1c05cce96d8344b5976a71e6caaf48e4
Allen <lcf7891@gmail.com>
2023-11-17 00:06:55 +0800

    建立與設定 scss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,10 @@
 <template>
   <main>
     <button type="button" class="btn btn-primary">Primary</button>
+    <h1 class="font-mantous h1">測試字體</h1>
+    <h2 class="h2 text-tertiary">測試字體 test font</h2>
+    <button type="button" class="btn btn-pill btn-info">測試 Btn <span class="btn-arrow"></span></button>
+    <h3 class="h1 text-gradient">漸變色</h3>
+    <h4 class="h1 text-gradient text-gradient-hover">動態漸變色</h4>
   </main>
 </template>
